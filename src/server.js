4b038a8d7c2f4e3f2124cf994d3add952b1a2823
Dương Đỗ <dustin.do95@gmail.com>
2020-07-15 20:16:04 +0700

    add filter, remove, edit, pagination

--- src/server.js
+++ src/server.js
@@ -9,14 +9,90 @@ const config = {
   serverPort: 4000,
 };
 
+/**
+ * 1. Filter task by done: GET /tasks?done=true|false
+ * 2. Remove a task by ID: DELETE /tasks/:taskId
+ * 3. Edit: PUT /tasks/:taskId
+ * 4. Pagination GET /tasks?page=1&perPage=10
+ */
+
 app.get("/tasks", (req, res) => {
-  Task.find()
-    .sort("-_id")
-    // or
-    // .sort({ _id: -1 })
-    .then((tasks) => {
-      res.json(tasks);
-    });
+  let { page = "1", perPage = "10" } = req.query;
+  page = +page;
+  perPage = +perPage;
+
+  let query = {};
+
+  if (req.query.done === "true") {
+    query.done = true;
+    // query = { done: true }
+  } else if (req.query.done === "false") {
+    query.done = false;
+    // query = { done: false }
+  }
+  
+  Task.countDocuments(query).then((total) => {
+    Task.find(query)
+      .sort("-_id")
+      .skip(perPage * (page - 1))
+      .limit(perPage)
+      .then((tasks) => {
+        res.json({
+          total,
+          page,
+          perPage,
+          ...query,
+          tasks,
+        });
+      });
+  });
+});
+
+app.delete("/tasks/:taskId", (req, res) => {
+  const { taskId } = req.params;
+
+  if (!mongoose.isValidObjectId(taskId)) {
+    res.status(400).json({ message: "TaskID is invalid" });
+    return;
+  }
+
+  Task.findByIdAndRemove(taskId).then((removedTask) => {
+    if (!removedTask) {
+      res.status(404).json({ message: "TaskID not found" });
+      return;
+    }
+    res.json(removedTask);
+  });
+});
+
+app.put("/tasks/:taskId", (req, res) => {
+  const { taskId } = req.params;
+  const { content, done } = req.query;
+
+  if (!mongoose.isValidObjectId(taskId)) {
+    res.status(400).json({ message: "TaskID is invalid" });
+    return;
+  }
+
+  let update = {};
+
+  if (content) {
+    update.content = content;
+  }
+  if (done === "true") {
+    update.done = true;
+  }
+  if (done === "false") {
+    update.done = false;
+  }
+
+  Task.findByIdAndUpdate(taskId, update, { new: true }).then((updatedTask) => {
+    if (!updatedTask) {
+      res.status(404).json({ message: "TaskID not found" });
+      return;
+    }
+    res.json(updatedTask);
+  });
 });
 
 app.post("/tasks", (req, res) => {
